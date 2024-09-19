2e2e68a7a8b5513697bd87e4b50213a250e7439c
Vimal Ram K <vimalramk99@gmail.com>
2024-09-19 19:45:52 +0530

    Fixed category component gap issue

--- src/components/category/homecatogary.tsx
+++ src/components/category/homecatogary.tsx
@@ -9,10 +9,14 @@ export const HomeCatagory = (props: HomeCatagoryType) => {
   return (
     <nav className={`${props.display}`}>
 <<<<<<< HEAD
+<<<<<<< HEAD
       <ul className=" container d-flex justify-content-lg-around justify-content-around align-items-center  column-gap-lg-5 p-0 py-2 py-lg-0 m-lg-0">
 =======
       <ul className=" d-flex justify-content-around align-items-center column-gap-md-4 column-gap-lg-5 p-0 py-2 py-lg-0 m-lg-0">
 >>>>>>> 8a7f19c (Design responsive landing page navigationbar layout)
+=======
+      <ul className=" container d-flex justify-content-lg-around justify-content-around align-items-center  column-gap-lg-5 p-0 py-2 py-lg-0 m-lg-0">
+>>>>>>> 84b0626 (Fixed category component gap issue)
         <li className=" list-unstyled">
           <Link
             to="/guest/articles"
